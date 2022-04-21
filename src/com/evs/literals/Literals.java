8e7e6156d2fde81532ccf19101b6b8a084c7e9e8
vinayYadav <vinay123@gmail.com>
2022-04-21 12:14:25 +0530

    rinku to commit

---
+++ src/com/evs/literals/Literals.java
@@ -0,0 +1,55 @@
+package com.evs.literals;
+
+public class Literals {
+	
+	public static void main(String[] args) {
+		literals();
+	}
+	
+		public static void literals() {
+		    
+			
+			/* baniary=======0b=========100001111
+		      octa===========0(zero)====*8
+		      hexa-decimal===0x=========*16
+		      decimal========normal=====normal
+		      
+		      decimal se octa,hexa-decimal me convert karne per divison(/) karnA PADATA HAI
+		      (example 32(decimal convert hexa decimal)/)
+		      32/16=20(hexa decimal)
+		      
+		      octa,hexa-decimal se decimal  me convert karne per multiply(*) karnA PADATA HAI
+		      
+		      long l=10l;
+			float f=10f;
+			double d=10d;
+
+		      */
+//			int i=10;
+//			int j=010;
+//			int k=0x10;
+//			System.out.println(i+j+k);
+//			                   //10+ 8+ 16=34
+			
+//			int s=0b1111000001111;
+//			System.out.println(s);
+			
+//			double f=0xBeef;    //BEEF
+//			System.out.println(f);
+			
+//			double d=10e2;             //10e2  ===exponential      10*10^2
+//			System.out.println(d);
+			
+//			float f=10e2f;
+//			System.out.println(f);
+			
+//			char c=0xea;
+//			System.out.println(c);
+			
+//			double d=123456.756;
+//			double e=1_23_345_765.23;
+//			System.out.println(e);
+			
+		}
+
+}
